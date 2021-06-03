2820c9fbce05f4d54c598109251e9fe1c996c10c
Zen-Hub-Exploits <85144919+Zen-Hub-Exploits@users.noreply.github.com>
2021-06-03 12:15:55 -0600

    Rename Jailbreak FLY AND INFINITE JUMP [FIXED MAY 2021].txt to Fly.lua

---
+++ Fly.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://gist.githubusercontent.com/uncooI/e59031055ab468d023c14fcf5faae993/raw/02067759e38a40f9a21bf24067c95ecbc8a052b2/jailbreak%2520fly%2520infinite%2520jump"))()
